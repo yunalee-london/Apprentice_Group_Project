0b15fa72599430a7acbf95bbf9d2e3b42d1f5bf4
Yuna Lee <yunalee@fb.com>
2020-10-14 22:41:11 +0100

    seed.js, server.js, models.js, views directory, styleallboards.css are updated

--- models.js
+++ models.js
@@ -1,6 +1,8 @@
 const { Sequelize, Model, DataTypes } = require('sequelize')
 const path = require('path')
-const sequelize = process.env.NODE_ENV === 'test' ? new Sequelize('sqlite::memory:', null, null, {dialect: 'sqlite'}) : new Sequelize({dialect: 'sqlite', storage: path.join(__dirname, 'data.db')})
+const sequelize = process.env.NODE_ENV === 'test' 
+    ? new Sequelize('sqlite::memory:', null, null, {dialect: 'sqlite'}) 
+    : new Sequelize({dialect: 'sqlite', storage: path.join(__dirname, 'data.db')})
 
 class User extends Model {}
 class ProjectBoard extends Model {}
@@ -19,18 +21,20 @@ ProjectBoard.init({
 }, {sequelize})
 
 List.init({
-    name: DataTypes.STRING,
-})
+    description: DataTypes.STRING,
+}, {sequelize})
 
 
-ProjectBoard.hasMany(List)
+ProjectBoard.hasMany(List, {as:"lists"})
 List.belongsTo(ProjectBoard)
-
+List.hasMany(User, {as:"users"})
+User.belongsTo(List)
 
 
 
 module.exports = {
     ProjectBoard,
     sequelize,
-    List
+    List,
+    User
 }
--- server.js
+++ server.js
@@ -1,4 +1,46 @@
 const express = require('express')
+const Handlebars = require('handlebars')
+const expressHandlebars = require('express-handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const app = express()
-const {ProjectBoard, List, sequelize} = require('./models')
+const {ProjectBoard, List, User, sequelize} = require('./models')
 
+const handlebars = expressHandlebars({
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+
+app.use(express.static('public'))
+app.engine('handlebars', handlebars)
+app.set('view engine', 'handlebars')
+
+app.get('/projectboards', async(req, res) => {
+    const projectboards = await ProjectBoard.findAll({
+        include : [
+            {model: List , as: 'lists'}
+        ]
+    })
+    res.render('projectboards', {projectboards})
+})
+
+app.get('/projectboards/:id', async(req, res) => {
+    const projectboard = await ProjectBoard.findByPk(req.params.id)
+    const lists = await projectboard.getLists({
+        include : ['lists']
+    })
+    res.render('lists', {projectboard, lists})
+})
+
+//add
+app.post('/projectboards', async(req, res) => {
+    await ProjectBoard.create(req.body)
+    res.redirect('/projectboards')
+})
+app.post('/projectboards/:id', async(req, res) => {
+    const projectboard = await ProjectBoard.findByPk(req.params.id)
+    await projectboard.createList(req.body)
+    res.redirect(`/projectboards/${projectboard.id}`)
+})
+
+
+
+app.listen(3001, () => console.log('web server running on port 3001'))
