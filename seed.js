7e012f8bfc95b42d4ee18e6ca9ed8f7b6475f79f
Yuna Lee <yunalee@fb.com>
2020-10-16 15:04:14 +0100

    seed.js

--- seed.js
+++ seed.js
@@ -2,43 +2,32 @@ const {ProjectBoard, List, User, sequelize, Task} = require('./models')
 const data = [
     {
         "name": "Spring Cleaning",
-        "lists": [
-            {
-                "name": "To Do",
-                "tasks": [
-                    {
-                        "description": "Hoover the floor"
-                    }
-                ]
-            },
-            {
-                "name": "Doing",
-                "tasks" : [
-                    {
-                        "description": "Clean the window"
-                    }
-                ]
-            },
-            {
-                "name": "Done",
-                "tasks": [
-                    {
-                        "description": "Take unused stuff to a charity shop"
-                    }
-                ]
-            }
+        "tasks": [
+            { "name": "Hoover the floor", "status": "available"},
+            { "name": "clean the window", "status": "available"},
+            { "name": "take rubbish out", "status": "available"}           
         ]
     }
 ]
+// sequelize.sync().then(async () => {
+//     const taskQueue = data.map(async (_projectboard) => {
+//             const projectboard = await ProjectBoard.create({name: _projectboard.name})
+//             const tasks = await Promise.all(_projectboard.tasks.map(async (_task) => {
+//                 const task = await Task.create({name: _task.name})
+//                 return list.setTasks(tasks)
+//             }))
+//             return await projectboard.sets(tasks)
+//         })
+//     await Promise.all(taskQueue).then(projectboards => {
+//         console.log(JSON.stringify(projectboards, null, 2))
+//     }).catch(console.error)
+// })
+
 sequelize.sync().then(async () => {
     const taskQueue = data.map(async (_projectboard) => {
             const projectboard = await ProjectBoard.create({name: _projectboard.name})
-            const lists = await Promise.all(_projectboard.lists.map(async (_list) => {
-                const tasks = await Promise.all(_list.tasks.map(({description}) => Task.create({description})))
-                const list = await List.create({name: _list.name})
-                return list.setTasks(tasks)
-            }))
-            return await projectboard.setLists(lists)
+            const tasks = await Promise.all(_projectboard.tasks.map(({name, status}) => Task.create({name, status})))
+            return await projectboard.setTasks(tasks)
         })
     await Promise.all(taskQueue).then(projectboards => {
         console.log(JSON.stringify(projectboards, null, 2))
